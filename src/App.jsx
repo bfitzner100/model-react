09014ae80b6ae968c68efb3f720586a96b5b8b07
bfitzner100 <brunofitzner1973@gmail.com>
2023-12-04 02:04:13 +0000

    Alterações de dados pessoais

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Bruno Fitzner do Nascimento Nascentes de Oliveira",
+  title: "FullStack Developer",
+  email: "brunofitzner.dev@gmail.com",
+  gitHub: "https://github.com/bfitzner100",
+  instagram: "@bruno_fitzner",
+  linkedIn: "https://www.linkedin.com/in/brunofitzner",
 };
 
 const primaryColor = "#4E567E";
